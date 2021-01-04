fc4dd6a2500b39935591a4a8352f896a6c104291
danil <danil.sheshe@gmail.com>
2021-01-04 17:08:46 +0300

    add D 74

--- D.cpp
+++ D.cpp
@@ -1,4 +1,4 @@
-﻿//#pragma GCC optimize("Ofast,unroll-loops")
+﻿//#pragma GCC optimize("Ofast")
  
 #ifdef LOCAL
 #include "debug.h"
@@ -27,7 +27,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 mt19937 rng(1000 - 7);
 #endif
  
-const int N = 1e5 + 10;
+const int N = 1e6 + 10;
 //const ll MOD = 998244353;
 const ll MOD1 = 1e9 + 7;
 const ll MOD2 = 1e9 + 9;
@@ -45,14 +45,14 @@ struct SuffixArray {
 
     SuffixArray(const string& str) {
         int n = str.size();
-        vector <vector <int>> cnt(max(n, 256)), cnt2(n);
+        vector <vector <int>> cnt(max(n, 4)), cnt2(n);
 
         for (int i = 0; i < n; i++) {
-            cnt[str[i]].push_back(i);
+            cnt[str[i] - 'a' + 1].push_back(i);
         }
 
         int ptr = 0, cls = 0;
-        for (int i = 0; i < 256; i++) {
+        for (int i = 0; i < 4; i++) {
             for (int pos : cnt[i]) {
                 s[ptr++] = pos;
                 c[pos] = cls;
@@ -67,8 +67,8 @@ struct SuffixArray {
                 cnt2[i].clear();
             }
 
-            for (int i = 0; i < n; i++) {
-                cnt[c[(i + pw) % n]].push_back(i);
+            for (int i = 0, j = (pw >= n ? 0 : pw); i < n; i++, j = (j + 1 == n ? 0 : j + 1)) {
+                cnt[c[j]].push_back(i);
             }
 
             for (int i = 0; i < n; i++) {
@@ -86,7 +86,11 @@ struct SuffixArray {
 
             cls = cnew[s[0]] = 0;
             for (int i = 1; i < n; i++) {
-                cls += !(c[s[i]] == c[s[i - 1]] && c[(s[i] + pw) % n] == c[(s[i - 1] + pw) % n]);
+                int x = s[i] + pw, y = s[i - 1] + pw;
+                if (x >= n) x -= n;
+                if (y >= n) y -= n;
+
+                cls += !(c[s[i]] == c[s[i - 1]] && c[x] == c[y]);
                 cnew[s[i]] = cls;
             }
 
@@ -98,44 +102,75 @@ struct SuffixArray {
 };
 
 struct Tree {
-    int t[N], used[N], timer = 1;
+    int t[N], used[N], timer = 1, sum = 0;
 
     void update(int i, int x) {
+        sum += x;
         for (; i < N; i |= i + 1) {
             if (used[i] < timer) {
-                t[i] = 0;
+                t[i] = x;
                 used[i] = timer;
             }
-
-            t[i] += x;
+            else {
+                t[i] += x;
+            }
         }
     }
 
     int get(int i) {
         int res = 0;
         for (; i >= 0; i = (i & (i + 1)) - 1) {
-            if (used[i] < timer) {
-                t[i] = 0;
-                used[i] = timer;
+            if (used[i] == timer) {
+                res += t[i];
             }
-
-            res += t[i];
         }
         return res;
     }
 
-    int get(int l, int r) {
-        return get(r) - (l ? get(l - 1) : 0);
+    inline int get_suff(int l) {
+        return sum - (l ? get(l - 1) : 0);
     }
 
-    void clear() {
+    inline void clear() {
         timer++;
+        sum = 0;
     }
-} tree, depth;
+} depth;
+
+template <typename T>
+struct Vector {
+    T a[N];
+    int end = 0;
+
+    T& operator[](int i) {
+        return a[i];
+    }
+
+    inline void push_back(const T& x) {
+        a[end++] = x;
+    }
+
+    inline T& back() {
+        return a[end - 1];
+    }
+
+    inline void pop_back() {
+        end--;
+    }
+
+    inline void clear() {
+        end = 0;
+    }
+
+    inline int size() {
+        return end;
+    }
+};
 
 const ll base = 228;
 
-ll pw1[N], pw2[N], g1[N], g2[N];
+ll pw1[N], g1[N];
+ll pw2[N], g2[N];
 
 int n, m;
 vector <int> g[N];
@@ -164,25 +199,31 @@ int dfs_centroid(int u, int m, int p = -1) {
     return u;
 }
 
-string cur;
-vector <ll> h1, h2;
+Vector <char> cur;
+Vector <ll> h1;
+Vector <ll> h2;
 
-int dfs_less(int u, int d = 1, int p = -1) {
-    cur += s[u];
+SuffixArray suff;
+vector <int> eq[N];
+
+int dfs_less(int u, int i, int p = -1) {
+    cur.push_back(s[u]);
     h1.push_back((h1.back() * base + (s[u] - 'a' + 1)) % MOD1);
     h2.push_back((h2.back() * base + (s[u] - 'a' + 1)) % MOD2);
 
-    int l = 0, r = min(sz(h1), sz(t)), m;
+    int l = 0, r = min(h1.size(), sz(t)), m;
+
+    if (s[u] != t.front()) {
+        r = 0;
+    }
+
     while (r - l > 1) {
         m = (l + r) / 2;
 
-        ll s1 = (h1.back() - (m == sz(h1) - 1 ? 0 : pw1[m + 1] * h1[sz(h1) - m - 2] % MOD1) + MOD1) % MOD1;
-        ll s2 = (h2.back() - (m == sz(h2) - 1 ? 0 : pw2[m + 1] * h2[sz(h2) - m - 2] % MOD2) + MOD2) % MOD2;
+        ll s1 = (h1.back() - (m == sz(h1) - 1 ? 0 : pw1[m + 1] * h1[sz(h1) - m - 2]) + MOD1 * MOD1) % MOD1;
+        ll s2 = (h2.back() - (m == sz(h2) - 1 ? 0 : pw2[m + 1] * h2[sz(h2) - m - 2]) + MOD2 * MOD2) % MOD2;
 
-        ll t1 = g1[m];
-        ll t2 = g2[m];
-
-        if (s1 == t1 && s2 == t2) {
+        if (s1 == g1[m] && s2 == g2[m]) {
             l = m;
         }
         else {
@@ -190,13 +231,12 @@ int dfs_less(int u, int d = 1, int p = -1) {
         }
     }
 
-    if (s[u] != t.front()) {
-        r = 0;
-    }
-
     int res;
-    if (r == min(sz(h1), sz(t))) {
-        res = 0;    
+    if (r == min(h1.size(), sz(t))) {
+        res = 0;
+        if (h1.size() <= sz(t)) {
+            eq[i].push_back(suff.c[h1.size()]);
+        }
     }
     else {
         res = (cur[sz(cur) - r - 1] < t[r]);
@@ -204,7 +244,7 @@ int dfs_less(int u, int d = 1, int p = -1) {
 
     for (int v : g[u]) {
         if (v != p && !used[v]) {
-            res += dfs_less(v, d + 1, u);
+            res += dfs_less(v, i, u);
         }
     }
 
@@ -215,40 +255,13 @@ int dfs_less(int u, int d = 1, int p = -1) {
     return res;
 }
 
-ll curans;
-SuffixArray suff;
-string str;
-
-void dfs_eq(int u, int mul, int p = -1, int d = 2) {
-    if (d == sz(str)) {
-        return;
-    }
-
-    h1.push_back((h1.back() * base + (s[u] - 'a' + 1)) % MOD1);
-    h2.push_back((h2.back() * base + (s[u] - 'a' + 1)) % MOD2);
-
-    int x = (h1.back() == g1[d - 1] && h2.back() == g2[d - 1]) * mul;
-
-    depth.update(suff.c[d], x);
-    curans += x * tree.get(suff.c[d]);
-
-    for (int v : g[u]) {
-        if (v != p && !used[v]) {
-            dfs_eq(v, mul, u, d + 1);
-        }
-    }
-
-    h1.pop_back();
-    h2.pop_back();
-}
-
-vector <pii> st;
+Vector <pii> st;
 
 inline char get(int i, int j) {
-    return (i + j < sz(str) ? str[i + j] : '$');
+    return (i + j < sz(t) ? t[i + j] : 96);
 }
 
-void dfs_add(int u, int flag, int p = -1) {
+ll dfs_add(int u, int p = -1) {
     auto [l, r] = st.back();
 
     int lb = l - 1, rb = r, mid;
@@ -264,7 +277,7 @@ void dfs_add(int u, int flag, int p = -1) {
     }
     int L = rb;
 
-    lb = l - 1, rb = r;
+    lb = L - 1, rb = r;
     while (rb - lb > 1) {
         mid = (lb + rb) / 2;
 
@@ -280,39 +293,21 @@ void dfs_add(int u, int flag, int p = -1) {
     //debug(st, L, R, s[u]);
 
     st.push_back({ L, R });
-    tree.update(L, flag);
-    curans += flag * depth.get(L, sz(str) - 1);
+    ll res = depth.get_suff(L);
 
     for (int v : g[u]) {
         if (v != p && !used[v]) {
-            dfs_add(v, flag, u); 
+            res += dfs_add(v, u); 
         }
     }
 
     st.pop_back();
-}
 
-int dfs_depth(int u, int p = -1, int d = 1) {
-    int res = d;
-    for (int v : g[u]) {
-        if (v != p && !used[v]) {
-            res = max(res, dfs_depth(v, u, d + 1));
-        }
-    }
     return res;
 }
 
 ll dfs_decomposition(int u) {
     used[u] = 1;
-
-    int d = 0;
-    for (int v : g[u]) {
-        if (!used[v]) {
-            d = max(d, dfs_depth(v));
-        }
-    }
-    int len = min(2 * d + 2, sz(t));
-    //debug(u, len, sz(t));
 
     int usize = 1;
     for (int v : g[u]) {
@@ -325,30 +320,26 @@ ll dfs_decomposition(int u) {
     ll ans = 0;
 
     cur.clear();
-    cur += s[u];
+    cur.push_back(s[u]);
 
-    h1.clear(), h2.clear();
-    h1.push_back(s[u] - 'a' + 1);
-    h2.push_back(s[u] - 'a' + 1);
+    h1.clear(), h1.push_back(s[u] - 'a' + 1);
+    h2.clear(), h2.push_back(s[u] - 'a' + 1);
 
-    str = t.substr(0, len) + '$';
-    suff = SuffixArray(str);
     st.clear();
-    st.push_back({ 0, len + 1 });
+    st.push_back({ 0, sz(t) + 1 });
 
-    curans = 0;
-    tree.clear();
     depth.clear();
 
     for (int v : g[u]) {
         if (!used[v]) {
-            ans += 1LL * dfs_less(v) * (usize - sz[v]);
-        }
-    }
+            eq[v].clear();
 
-    for (int v : g[u]) {
-        if (!used[v]) {
-            dfs_eq(v, 1);
+            ans += 1LL * dfs_less(v, v) * (usize - sz[v]);
+            ans += sz(eq[v]);
+
+            for (int x : eq[v]) {
+                depth.update(x, 1);
+            }
         }
     }
 
@@ -357,19 +348,20 @@ ll dfs_decomposition(int u) {
     }
     else if (s[u] == t.front()) {
         depth.update(suff.c[1], 1);
+        ans++;
     }
-
-    ans += depth.get(sz(str) - 1);
 
     for (int v : g[u]) {
         if (!used[v]) {
-            dfs_eq(v, -1);
-            dfs_add(v, 1);
+            for (int x : eq[v]) {
+                depth.update(x, -1);
+            }
 
-            ans += curans;
+            ans += dfs_add(v);
 
-            dfs_add(v, -1);
-            dfs_eq(v, 1);
+            for (int x : eq[v]) {
+                depth.update(x, 1);
+            }
         }
     }
 
@@ -377,7 +369,7 @@ ll dfs_decomposition(int u) {
 
     for (int v : g[u]) {
         if (!used[v]) {
-            ans += dfs_decomposition(dfs_centroid(v, s[v]));
+            ans += dfs_decomposition(dfs_centroid(v, sz[v]));
         }
     }
     
@@ -392,7 +384,8 @@ signed main() {
     cout << fixed << setprecision(9);
     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 
-    pw1[0] = pw2[0] = 1;
+    pw1[0] = 1;
+    pw2[0] = 1;
     for (int i = 1; i < N; i++) {
         pw1[i] = pw1[i - 1] * base % MOD1;
         pw2[i] = pw2[i - 1] * base % MOD2;
@@ -400,7 +393,11 @@ signed main() {
 
     cin >> n >> m >> t;
 
-    g1[0] = g2[0] = (t.front() - 'a' + 1);
+    t += char(96);
+    suff = SuffixArray(t);
+
+    g1[0] = (t.front() - 'a' + 1);
+    g2[0] = (t.front() - 'a' + 1);
     for (int i = 1; i < m; i++) {
         g1[i] = (g1[i - 1] + (t[i] - 'a' + 1) * pw1[i]) % MOD1;
         g2[i] = (g2[i - 1] + (t[i] - 'a' + 1) * pw2[i]) % MOD2;
